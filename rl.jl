50e5b9740d36fe21a488e941506e1dc1284a752a
Beryl Aribowo <berylramadhian@gmail.com>
2023-02-07 19:21:06 +0100

    added fvmc and test case

--- rl.jl
+++ rl.jl
@@ -15,12 +15,37 @@ function evmc(states, S, R, γ)
     for i ∈ eachindex(S) # foreach episode
         G = 0. # reset G foreach eps
         # compute the return G backwards:
-        println("episode",i)
+        println("episode ",i," :")
         for t ∈ reverse(eachindex(S[i]))
             G = γ*G + R[i][t] # discount G_{t+1}
             n[S[i][t]] += 1 # update counter
             Vs[S[i][t]] += G # update return of S_t
             println([t, S[i][t], n[S[i][t]], G])
+        end
+    end
+    display(n)
+    display(Vs)
+    return Vs ./ n # average V(s) := V(s)/n(s)
+end
+
+"""
+first visit MC, return at first occurence
+"""
+function fvmc(states, S, R, γ)
+    n = zeros(Int, length(states)) # n_ts[i] := counter of visiting state i
+    Vs = zeros(length(states)) # output
+    for i ∈ eachindex(S) # foreach episode
+        G = 0. # reset G foreach eps
+        # compute the return G backwards:
+        println("episode ",i," :")
+        for t ∈ reverse(eachindex(S[i]))
+            G = γ*G + R[i][t] # discount G_{t+1}
+            # for first exit MC, check if S[t] is in S[1:t-1]:
+            if S[i][t] ∉ S[i][1:t-1]
+                n[S[i][t]] += 1 # update counter
+                Vs[S[i][t]] += G # update return of S_t
+            end
+            println([t, S[i][t], n[S[i][t]], G, S[i][1:t-1], S[i][t] ∉ S[i][1:t-1]])
         end
     end
     display(n)
@@ -54,6 +79,8 @@ function main()
     trjs = [Dict("s"=>1, "a"=>1, "r"=>1.)]
     # initialize learning rates:
     γ = .5; α = .1
-    Vs = evmc(["A", "B"], S, R, γ)
-    display(Vs)
+    Vs = evmc(["A", "B"], S, R, γ)  # evmc caller
+    println("v^π(s) from every-visit-MC is ",Vs)
+    Vs = fvmc(["A", "B"], S, R, γ)  # fvmc caller
+    println("v^π(s) from first-visit-MC is ",Vs)
 end
